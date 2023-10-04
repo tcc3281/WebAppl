900db3889e602cc03a2415c47eb0468a5d4928e6
tcc3281 <tcc3281@gmail.com>
2023-10-04 20:00:00 +0700

    Add project files.

---
+++ Controllers/LearnerController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using WebAppl.Data;
+using WebAppl.Models;
+
+namespace WebAppl.Controllers
+{
+    public class LearnerController : Controller
+    {
+        private SchoolContext db;
+        public LearnerController(SchoolContext context)
+        {
+            db = context;
+        }
+        public IActionResult Index()
+        {
+            var learners = db.Learners.Include(m => m.Major).ToList();
+            return View(learners);
+        }
+        public IActionResult Create()
+        {
+            //dùng 1 trong 2 cách để tạo SelectList gửi về View qua ViewBag để
+            //hiển thị danh sách chuyên ngành (Majors)
+            /*var majors = new List<SelectListItem>(); //cách 1
+            foreach (var item in db.Majors)
+            {
+                majors.Add(new SelectListItem
+                {
+                    Text = item.MajorName,
+                    Value = item.MajorID.ToString()
+                });
+            }
+            ViewBag.MajorID = majors;*/
+            ViewBag.MajorID = new SelectList(db.Majors, "MajorID", "MajorName"); //cách 2
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create([Bind("FirstMidName,LastName,MajorID,EnrollmentDate")] Learner learner)
+        {
+            StreamWriter writer = new StreamWriter(".\\Data\\text.txt");
+            writer.WriteLine(learner.MajorID.ToString());
+            writer.WriteLine(learner.EnrollmentDate.ToString());
+            writer.Close();
+            if (ModelState.IsValid)
+            {
+                db.Learners.Add(learner);
+                db.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            //lại dùng 1 trong 2 cách tạo SelectList gửi về View để hiển thị danh sách Majors
+            else
+            {
+                ViewBag.MajorID = new SelectList(db.Majors, "MajorID", "MajorName");
+                return View();
+            }
+        }
+    }
+}
